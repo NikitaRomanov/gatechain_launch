610ea950cf8ace9c07c9064684d0e7f52021b5e7
timofeykulakov <kulakov.timofey.1997@gmail.com>
2022-04-24 19:59:21 +0300

    run_priv version

---
+++ run_priv/run_evm_rpc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+until [ -f ${GATEHOME}/api.token ]
+do
+	echo "wait for ${GATEHOME}/api.token"
+	sleep 1
+done
+cp ${GATEHOME}/api.token ${GATEHOME}/../.gatecli/
+gatecli evm rest-server --gm-websocket-port http://127.0.0.1:8081 --chain-id testing-1 --laddr tcp://0.0.0.0:6060 --rpc-api web3,eth,personal,net,debug
+#address=$(jq '.address' ${GATEHOME}/acc_log.json)
+#gatecli con-account create --indent --output json `echo ${address:1:-1}` --chain-id mainnet &> ${GATEHOME}/con_acc_log.json
+#gatecli evm rest-server --gm-websocket-port http://127.0.0.1:8081 \
+#	--chain-id mainnet \
+#	--laddr tcp://0.0.0.0:6060 \
+#	--rpc-api web3,eth,personal,net,debug
