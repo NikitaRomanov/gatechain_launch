808b4d02de2532abc92d0d0d455eb892fd4ad6de
TimofeyKulakov <55335226+TimofeyKulakov@users.noreply.github.com>
2022-04-27 01:47:56 +0300

    Private chain (#3)
    * run_priv version
    
    * run_priv removed comments
    
    * run_priv remastered
    
    * Update README.md
    
    * chain name added
    
    * Update README.md
    
    Co-authored-by: timofeykulakov <kulakov.timofey.1997@gmail.com>

--- run_evm_rpc.sh
+++ run_evm_rpc.sh
@@ -6,8 +6,8 @@ do
   sleep 1
 done
 cp ${GATEHOME}/api.token ${GATEHOME}/../.gatecli/
-
-if [ ${CHAIN} = "mainnet"]; then
+echo ${CHAIN}
+if [ ${CHAIN} = 'mainnet']; then
 	address=$(jq '.address' ${GATEHOME}/acc_log.json)
 	gatecli con-account create --indent --output json `echo ${address:1:-1}` --chain-id mainnet &> ${GATEHOME}/con_acc_log.json
 	gatecli evm rest-server --gm-websocket-port http://127.0.0.1:8081 \
@@ -15,5 +15,5 @@ if [ ${CHAIN} = "mainnet"]; then
                         --laddr tcp://0.0.0.0:6060 \
                         --rpc-api web3,eth,personal,net,debug
 else
-	gatecli evm rest-server --gm-websocket-port http://127.0.0.1:8081 --chain-id testing-1 --laddr tcp://0.0.0.0:6060 --rpc-api web3,eth,personal,net,debug
+        gatecli evm rest-server --gm-websocket-port http://127.0.0.1:8081 --chain-id ${CHAIN} --laddr tcp://0.0.0.0:6060 --rpc-api web3,eth,personal,net,debug
 fi
